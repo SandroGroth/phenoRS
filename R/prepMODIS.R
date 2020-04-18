14a10d7ac0013a81ba9905a7dafd15d72763764a
SandroGroth <sandro_groth@outlook.com>
2020-04-18 16:21:52 +0200

    Reimplements prepMODIS

--- R/prepMODIS.R
+++ R/prepMODIS.R
@@ -1,6 +1,18 @@
+#'
+#' @import doParallel
+#' @import parallel
+#'
+#' @importFrom foreach foreach
+#' @importFrom gdalUtils gdal_translate gdalwarp gdalbuildvrt mosaic_rasters
+#' @importFrom logging loginfo
+#' @importFrom MODIS getSds
+#' @importFrom raster raster stack rasterTmpFile removeTmpFiles extension extent writeRaster
+#'
+#' @export
+#'
 prepMODIS <- function(in_dir, out_dir, aoi, vi='NDVI', out_proj=NA, cores=NA) {
 
-  # check if input parameters are valied on the first look
+  # check if input parameters are valid on the first look
   valid_VIs <- c('NDVI', 'EVI')
   if (!(all(vi %in% valid_VIs))) stop("Specified vi is not supported.")
   if (!dir.exists(in_dir)) stop("Input directory not found.")
@@ -13,29 +25,9 @@ prepMODIS <- function(in_dir, out_dir, aoi, vi='NDVI', out_proj=NA, cores=NA) {
 
   logging::loginfo("Starting preprocessing...")
 
-  # setup data structure:
-  # out_dir
-  # |- [NDVI]
-  # |- [EVI]
-  # |- QA
-  # |- DOY
+  # setup data structure
   dir.create(out_dir)
-  if ('NDVI' %in% vi) {
-    ndvi_dir <- file.path(out_dir, 'NDVI')
-    dir.create(ndvi_dir)
-  }
-  if ('EVI' %in% vi) {
-    evi_dir <- file.path(out_dir, 'EVI')
-    dir.create(evi_dir)
-  }
-  qa_dir <- file.path(out_dir, 'QA')
-  doy_dir <- file.path(out_dir, 'DOY')
-  dir.create(qa_dir)
-  dir.create(doy_dir)
   logging::loginfo(paste0("Output directory set: ", out_dir))
-
-  # Get all subdataset names that should be processed based on the created directories
-  sds <- list.dirs(out_dir, full.names = FALSE, recursive = FALSE)
 
   # set up parallel processing based on number of available or specified cores
   if (is.na(cores)) cores <- parallel::detectCores() -1
@@ -52,161 +44,119 @@ prepMODIS <- function(in_dir, out_dir, aoi, vi='NDVI', out_proj=NA, cores=NA) {
   temp_aoi <- file.path(tempdir(), "aoi_mask.shp")
   sf::st_write(aoi, temp_aoi, overwrite = TRUE, delete_dsn = TRUE, quiet = TRUE)
 
-  # Loop trough all sds and execute preprocessing steps
-  for (i in 1:length(sds)) {
-    sd <- sds[i]
-    sd_out_dir <- file.path(out_dir, sd)
-    sd_idx <- .get_sd_idx(hdf_files[1], sd)
-    out_ot <- 'Int16'
-    logging::loginfo(paste0("Processing subdataset ", i, " of ", length(sds), ": ", sd))
+  # ---- SDS Extraction ----
+  logging::loginfo("Extracting subdatasets from .hdf files...")
+  foreach::foreach(f = 1:length(hdf_files), .packages = c("raster", "gdalUtils", "MODIS")) %dopar% {
+    out_file <- file.path(out_dir, paste0(strsplit(basename(hdf_files[f]), ".hdf")[[1]][1], "_extract.tif"))
+    sds <- MODIS::getSds(hdf_files[f])[[2]]
 
-    # ---- SDS Extraction ----
-    logging::logdebug("Extracting subdataset from .hdf archives...")
-    foreach::foreach(f = 1:length(hdf_files), .packages = "gdalUtils") %dopar% {
-      out_file <- file.path(sd_out_dir, paste0(strsplit(basename(hdf_files[f]), ".hdf")[[1]][1], "_", sd, ".tif"))
-      gdalUtils::gdal_translate(hdf_files[f], out_file, sd_index = sd_idx, of = 'GTiff', ot = out_ot)
+    temp_env_vi <- raster::rasterTmpFile()
+    temp_env_doy <- raster::rasterTmpFile()
+    temp_env_qa <- raster::rasterTmpFile()
+
+    raster::extension(temp_env_vi) <- ".tif"
+    raster::extension(temp_env_doy) <- ".tif"
+    raster::extension(temp_env_qa) <- ".tif"
+
+    gdalUtils::gdal_translate(grep(vi, sds, value = TRUE), dst_dataset = temp_env_vi)
+    gdalUtils::gdal_translate(grep("day of the year", sds, value = TRUE), dst_dataset = temp_env_doy)
+    gdalUtils::gdal_translate(grep("pixel reliability", sds, value = TRUE), dst_dataset = temp_env_qa)
+
+    r_vi <- raster::raster(temp_env_vi) / 10000 # TODO: check value range for correct rescaling
+    r_doy <- raster::raster(temp_env_doy)
+    r_qa <- raster::raster(temp_env_qa)
+
+    r_stack <- raster::stack(list(r_vi, r_doy, r_qa))
+    names(r_stack) <- c(vi, "DOY", "QA")
+
+    writeRaster(r_stack, filename = out_file, datatype = 'INT2U', format = 'GTiff', overwrite = TRUE)
+  }
+  raster::removeTmpFiles(h=0)
+
+  # ---- Tile Mosaicing ----
+  logging::loginfo("Mosaicing tiles...")
+  img_files <- list.files(out_dir, '.*M(O|Y)D.*_extract.tif', full.names = TRUE, no.. = TRUE)
+
+  if (length(.get_unique_tilestr(img_files)) > 1) {
+    dates <- unique(do.call("c", lapply(img_files, .getMODIS_date)))
+
+    foreach::foreach(f = 1:length(dates), .packages = "gdalUtils") %dopar% {
+      date_curr_str <- strftime(dates[f], format = '%Y%j')
+      date_curr_files <- img_files[grepl(paste0("\\.A", date_curr_str, "\\."), img_files)]
+      product_name <- strsplit(basename(date_curr_files[1]), "\\.")[[1]][1]
+      out_file <- file.path(out_dir, paste0(product_name, ".", date_curr_str, "_mosaic.tif"))
+      gdalUtils::mosaic_rasters(date_curr_files, out_file, of = 'GTiff', ot = 'UInt16')
     }
-
-    # ---- Tile Mosaicing ----
-
-    img_files <- list.files(sd_out_dir, paste0(".*M(O|Y)D.*_", sd, ".tif"), full.names = TRUE, no.. = TRUE)
-    if (length(.get_unique_tilestr(img_files)) > 1) {
-      dates <- unique(do.call("c", lapply(img_files, .getMODIS_date)))
-
-      logging::logdebug("Mosaicing MODIS tiles...")
-      foreach::foreach(f = 1:length(dates), .packages = "gdalUtils") %dopar% {
-        date_curr_str <- strftime(dates[f], format = '%Y%j')
-        date_curr_files <- img_files[grepl(paste0("\\.A", date_curr_str, "\\."), img_files)]
-        product_name <- strsplit(basename(date_curr_files[1]), "\\.")[[1]][1]
-        out_file <- file.path(sd_out_dir, paste0(product_name, ".", date_curr_str, "_", sd, "_", "mosaic.tif"))
-        gdalUtils::mosaic_rasters(date_curr_files, out_file, of = 'GTiff', ot = out_ot)
-      }
-    } else { # just rename to pretend mosaicing was done
-      logging::logdebug("Skipping mosaicing since only one unique tile found.")
-      for (j in 1:length(img_files)) {
-        product_name <- strsplit(basename(img_files[j]), '\\.')[[1]][1]
-        date_str <- .getMODIS_datestr(.getMODIS_date(basename(img_files[j])))
-        out_file <- file.path(sd_out_dir, paste0(product_name, ".", date_str, "_", sd, "_", "mosaic.tif"))
-        file.rename(from = img_files[j], to = out_file)
-      }
+  } else {
+    # just rename to pretend mosaicing was done
+    logging::loginfo("Skipped mosaicing since only one unique tile found.")
+    for (i in 1:length(img_files)) {
+      product_name <- strsplit(basename(img_files[i]), '\\.')[[1]][1]
+      date_str <- .getMODIS_datestr(.getMODIS_date(basename(img_files[i])))
+      out_file <- file.path(out_dir, paste0(product_name, ".", date_str, "_mosaic.tif"))
+      file.rename(from = img_files[i], to = out_file)
     }
+  }
 
-    # cleanup
-    do.call(file.remove, list(list.files(sd_out_dir, paste0(".*M(O|Y)D.*_", sd, ".tif"), full.names = TRUE, no.. = TRUE)))
+  # cleanup
+  do.call(file.remove, list(list.files(out_dir, paste0(".*M(O|Y)D.*_extract.tif"), full.names = TRUE, no.. = TRUE)))
 
-    # ---- Image Reprojection ----
+  # ---- Image Reprojection ----
 
-    if (!is.na(out_proj)) {
-      img_files <- list.files(sd_out_dir, paste0('.*M(O|Y)D.*_', sd, '_mosaic.tif'), full.names = TRUE, no.. = TRUE)
+  if (!is.na(out_proj)) {
+    img_files <- list.files(out_dir, paste0('.*M(O|Y)D.*_mosaic.tif'), full.names = TRUE, no.. = TRUE)
 
-      logging::logdebug(paste0("Reprojecting images to: ", out_proj))
-      foreach::foreach(f = 1:length(img_files), .packages = 'gdalUtils') %dopar% {
-        out_file <- file.path(sd_out_dir, paste0(strsplit(basename(img_files[f]), "_")[[1]][1], "_", sd, "_proj.tif"))
-        gdalUtils::gdalwarp(img_files[f], out_file, t_srs = out_proj, of = 'GTiff')
-      }
-
-      # cleanup
-      do.call(file.remove, list(list.files(sd_out_dir, paste0(".*M(O|Y)D.*", "_mosaic.tif"), full.names = TRUE, no.. = TRUE)))
-    }
-
-    # ---- AOI Cropping ----
-
-    if (!is.na(out_proj)) {
-      img_files <- list.files(sd_out_dir, paste0('.*M(O|Y)D.*_', sd, '_proj.tif'), full.names = TRUE, no.. = TRUE)
-    } else {
-      img_files <- list.files(sd_out_dir, paste0('.*M(O|Y)D.*_', sd, '_mosaic.tif'), full.names = TRUE, no.. = TRUE)
-    }
-    aoi_ext <- try(raster::extent(aoi))
-    if (class(aoi_ext) == 'try-error') stop(paste0("Unable to retrieve extent from aoi: ", as.character(aoi_ext[1])))
-
-    logging::logdebug("Cropping images to AOI...")
-    foreach::foreach(f = 1:length(img_files), .packages = c('gdalUtils', 'raster')) %dopar% {
-      out_file <- file.path(sd_out_dir, paste0(strsplit(basename(img_files[f]), "_")[[1]][1], "_", sd, "_crop.tif"))
-      temp_env <- tempfile(fileext = ".vrt")
-      catch <- gdalUtils::gdalbuildvrt(img_files[f], temp_env, te = c(aoi_ext@xmin, aoi_ext@ymin, aoi_ext@xmax, aoi_ext@ymax))
-      x <- raster::stack(temp_env)
-      raster::writeRaster(x, out_file, format = 'GTiff')
-    }
-
-    # cleanup
-    if (!is.na(out_proj)) {
-      do.call(file.remove, list(list.files(sd_out_dir, paste0(".*M(O|Y)D.*", "_proj.tif"), full.names = TRUE, no.. = TRUE)))
-    } else {
-      do.call(file.remove, list(list.files(sd_out_dir, paste0(".*M(O|Y)D.*", "_mosaic.tif"), full.names = TRUE, no.. = TRUE)))
-    }
-
-    # ---- AOI Masking ----
-
-    img_files <- list.files(sd_out_dir, paste0('.*M(O|Y)D.*_', sd, '_crop.tif'), full.names = TRUE, no.. = TRUE)
-
-    logging::logdebug("Masking images to AOI...")
+    logging::loginfo(paste0("Reprojecting images to: ", out_proj))
     foreach::foreach(f = 1:length(img_files), .packages = 'gdalUtils') %dopar% {
-      if (sd == 'QA') {
-        out_file <- file.path(sd_out_dir, paste0(strsplit(basename(img_files[f]), "_")[[1]][1], "_", sd, "_prep.tif"))
-      } else {
-        out_file <- file.path(sd_out_dir, paste0(strsplit(basename(img_files[f]), "_")[[1]][1], "_", sd, "_mask.tif"))
-      }
-      gdalUtils::gdalwarp(img_files[f], out_file, cutline = temp_aoi, of = 'GTiff')
+      out_file <- file.path(out_dir, paste0(strsplit(basename(img_files[f]), "_")[[1]][1], "_proj.tif"))
+      gdalUtils::gdalwarp(img_files[f], out_file, t_srs = out_proj, of = 'GTiff', ot = 'UInt16')
     }
 
     # cleanup
-    do.call(file.remove, list(list.files(sd_out_dir, paste0(".*M(O|Y)D.*", "_crop.tif"), full.names = TRUE, no.. = TRUE)))
+    do.call(file.remove, list(list.files(out_dir, paste0(".*M(O|Y)D.*_mosaic.tif"), full.names = TRUE, no.. = TRUE)))
+  } else {
+    logging::loginfo("Skipped image reprojection since no out_proj specified.")
   }
 
-  # ---- Pixel Reliability Masking ----
+  # ---- AOI Cropping ----
 
-  logging::loginfo("---------------------------------------------------------------------------------------------")
-  logging::loginfo("Starting Layer corrections...")
-  qa_files <- list.files(qa_dir, pattern = '.*M(O|Y)D.*_QA_prep.tif', full.names = TRUE, no.. = TRUE)
+  if (!is.na(out_proj)) {
+    img_files <- list.files(out_dir, paste0('.*M(O|Y)D.*_proj.tif'), full.names = TRUE, no.. = TRUE)
+  } else {
+    img_files <- list.files(out_dir, paste0('.*M(O|Y)D.*_mosaic.tif'), full.names = TRUE, no.. = TRUE)
+  }
+  aoi_ext <- try(raster::extent(aoi))
+  if (class(aoi_ext) == 'try-error') stop(paste0("Unable to retrieve extent from aoi: ", as.character(aoi_ext[1])))
 
-  # loop trough all extracted VIs an DOY
-  ds <- c(vi, "DOY")
-  for (i in 1:length(ds)) {
-    img_files <- list.files(file.path(out_dir, ds[i]), pattern=paste0('.*M(O|Y)D.*_', ds[i], '_mask.tif'),
-                            full.names = TRUE, no.. = TRUE)
-
-    logging::logdebug(paste0("Masking images based on pixel reliability for ", ds[i], "..."))
-    foreach::foreach(f = 1:length(qa_files), .packages = 'raster') %dopar% {
-      if (ds[i] == 'DOY') {
-        out_file <- file.path(out_dir, ds[i], paste0(strsplit(basename(qa_files[f]), "_")[[1]][1], "_", ds[i], "_prep.tif"))
-      } else {
-        out_file <- file.path(out_dir, ds[i], paste0(strsplit(basename(qa_files[f]), "_")[[1]][1], "_", ds[i], "_qmask.tif"))
-      }
-      img <- grep(strsplit(basename(qa_files[f]), '_')[[1]][1], img_files, value = TRUE)[1]
-      r <- raster::raster(img)
-      qa <- raster::raster(qa_files[f])
-
-      # all pixels with QA flag 2 = Snow/Ice and 3 = Cloudy will be set to NA
-      r[qa == 2] <- raster::NAvalue(r)
-      r[qa == 3] <- raster::NAvalue(r)
-
-      raster::writeRaster(r, out_file, format = 'GTiff')
-    }
-
-    # cleanup
-    do.call(file.remove, list(list.files(file.path(out_dir, ds[i]), paste0(".*M(O|Y)D.*", "_mask.tif"),
-                                         full.names = TRUE, no.. = TRUE)))
+  logging::loginfo("Cropping images to AOI...")
+  foreach::foreach(f = 1:length(img_files), .packages = c('gdalUtils', 'raster')) %dopar% {
+    out_file <- file.path(out_dir, paste0(strsplit(basename(img_files[f]), "_")[[1]][1], "_crop.tif"))
+    temp_env <- tempfile(fileext = ".vrt")
+    catch <- gdalUtils::gdalbuildvrt(img_files[f], temp_env, te = c(aoi_ext@xmin, aoi_ext@ymin, aoi_ext@xmax, aoi_ext@ymax))
+    x <- raster::stack(temp_env)
+    raster::writeRaster(x, out_file, format = 'GTiff', datatype = 'INT2U')
   }
 
-  # ---- VI Rescaling ----
-
-  # loop through all extracted VIs
-  for (i in 1:length(vi)) {
-
-    img_files <- list.files(file.path(out_dir, vi[i]), pattern=paste0('.*M(O|Y)D.*_', vi[i], '_qmask.tif'),
-                            full.names = TRUE, no.. = TRUE)
-    logging::logdebug(paste0("Rescaling VI for: ", vi[i]))
-    foreach::foreach(f = 1:length(img_files), .packages = 'raster') %dopar% {
-      out_file <- file.path(out_dir, vi[i], paste0(strsplit(basename(qa_files[f]), "_")[[1]][1], "_", ds[i], "_prep.tif"))
-      r <- raster::raster(img_files[f])
-      values(r) <- values(r) * 0.0001
-      raster::writeRaster(r, out_file, format = 'GTiff')
-    }
-
-    # cleanup
-    do.call(file.remove, list(list.files(file.path(out_dir, vi[i]), paste0(".*M(O|Y)D.*", "_qmask.tif"),
-                                         full.names = TRUE, no.. = TRUE)))
+  # cleanup
+  if (!is.na(out_proj)) {
+    do.call(file.remove, list(list.files(out_dir, paste0(".*M(O|Y)D.*_proj.tif"), full.names = TRUE, no.. = TRUE)))
+  } else {
+    do.call(file.remove, list(list.files(out_dir, paste0(".*M(O|Y)D.*_mosaic.tif"), full.names = TRUE, no.. = TRUE)))
   }
+
+  # ---- AOI Masking ----
+
+  img_files <- list.files(out_dir, paste0('.*M(O|Y)D.*_crop.tif'), full.names = TRUE, no.. = TRUE)
+
+  logging::loginfo("Masking images to AOI...")
+  foreach::foreach(f = 1:length(img_files), .packages = 'gdalUtils') %dopar% {
+    out_file <- file.path(out_dir, paste0(strsplit(basename(img_files[f]), "_")[[1]][1], "_prep.tif"))
+    gdalUtils::gdalwarp(img_files[f], out_file, cutline = temp_aoi, of = 'GTiff', ot = 'Uint16')
+  }
+
+  # cleanup
+  do.call(file.remove, list(list.files(out_dir, paste0(".*M(O|Y)D.*", "_crop.tif"), full.names = TRUE, no.. = TRUE)))
+
 
   # stop the multicore processing cluster
   parallel::stopCluster(par_cluster)
