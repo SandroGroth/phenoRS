027318f2f64c22c554840c11839597a05dd4907f
SandroGroth <sandro_groth@outlook.com>
2020-05-27 16:31:21 +0200

    Adds DOY layer interpolation.

--- R/checkTS.R
+++ R/checkTS.R
@@ -1,10 +1,18 @@
-check_ts <- function(v, max_data_gap, internal_min) {
+check_vi_ts <- function(v, max_data_gap, internal_min) {
 
   # Interpolate NA value, when the data gap is not greater than specified
-  v <- zoo::na.approx(v, maxgap = max_data_gap)
+  v <- zoo::na.approx(v, maxgap = max_data_gap) # VI
 
   # Assign internal_min to gaps greater than max_data_gap
   v[is.na(v)] <- internal_min
 
   v
+}
+
+check_doy_ts <- function(d) {
+
+  # Interpolate NA value
+  d <- zoo::na.approx(d)
+
+  d
 }
