2f331bbf0a404d12c2025225b9516e5065fb2232
SandroGroth <sandro_groth@outlook.com>
2020-06-04 10:52:32 +0200

    Refactored DOY correction.

--- R/fitTS.R
+++ R/fitTS.R
@@ -1,9 +1,9 @@
-fitTS <- function(prep_dir, out_dir) {
+fitTS <- function(prep_dir, out_dir, config) {
 
   # ---- Input Checks ----
 
   # Check if output directory already exists, otherwise create it
-  if (!dir.exists(out_dir)) dir.create(out_dir) else stop("Output directory already exists.")
+  #if (!dir.exists(out_dir)) dir.create(out_dir) else stop("Output directory already exists.")
 
   # ---- Metadata Handling ----
 
@@ -21,7 +21,7 @@ fitTS <- function(prep_dir, out_dir) {
     stop("Number of rows does not match.")
   }
   if (!identical(meta_vi$samples, meta_doy$samples) | !identical(meta_vi$samples, meta_qa$samples)) {
-    stop("Number of columns does not match.")
+    stop("Number of columns do not match.")
   }
 
   n_rows <- meta_vi$lines
@@ -29,25 +29,33 @@ fitTS <- function(prep_dir, out_dir) {
 
   # ---- Datacube Construction ----
 
-  # create connections for byte-streaming the image files
+  # list all prepared files
   bin_vi_files  <- list.files(prep_dir, pattern = '.*_(NDVI|EVI)_.*.envi$', full.names = TRUE, no.. = TRUE)
   bin_doy_files <- list.files(prep_dir, pattern = '.*_DOY_.*.envi$', full.names = TRUE, no.. = TRUE)
   bin_qa_files  <- list.files(prep_dir, pattern = '.*_QA_.*.envi$', full.names = TRUE, no.. = TRUE)
 
+  # check if the lengths of the prepared files are equal
+  if (isFALSE(length(unique(c(length(bin_vi_files), length(bin_doy_files), length(bin_qa_files)))) == 1)){
+    stop("Number of subdatasets (VI, DOY, QA) do not match.")
+  }
+
+  # get the base years from the filenames
+  years <- .get_prep_year_str(bin_vi_files)
+
   # create datacubes
   dc_vi  <- raster::brick(sapply(bin_vi_files, function(x) {
     r <- raster::raster(x)
-    names(r) <- strsplit(basename(x), '_')[[1]][1]
+    names(r) <- basename(x)
     return(r)}))
   names(dc_vi) <- unlist(lapply(bin_vi_files, function (x) {strsplit(basename(x), '_')[[1]][1]}))
   dc_doy <- raster::brick(sapply(bin_doy_files, function(x) {
     r <- raster::raster(x)
-    names(r) <- strsplit(basename(x), '_')[[1]][1]
+    names(r) <- basename(x)
     return(r)}))
   names(dc_doy) <- unlist(lapply(bin_doy_files, function (x) {strsplit(basename(x), '_')[[1]][1]}))
   dc_qa  <- raster::brick(sapply(bin_qa_files, function(x) {
     r <- raster::raster(x)
-    names(r) <- strsplit(basename(x), '_')[[1]][1]
+    names(r) <- basename(x)
     return(r)}))
   names(dc_qa) <- unlist(lapply(bin_qa_files, function (x) {strsplit(basename(x), '_')[[1]][1]}))
 
@@ -76,23 +84,66 @@ fitTS <- function(prep_dir, out_dir) {
       if (all(is.na(pix_vi))) next
 
       # -- Date Extraction / Correction --
-      # TODO: try to fix quick and dirty data wrestling below
-      pix_years <- as.integer(substr(names(pix_doy), 2, 5))
+
+      # load vectors
+      pix_years <- years
       pix_doy <- unname(pix_doy)
-      pix_years[!is.na(pix_doy) & lubridate::leap_year(pix_years) & pix_doy > 366] <- pix_years[
-        !is.na(pix_doy) & lubridate::leap_year(pix_years) & pix_doy > 366] + 1
-      pix_doy[!is.na(pix_doy) & lubridate::leap_year(pix_years) & pix_doy > 366] <- pix_doy[
-        !is.na(pix_doy) & lubridate::leap_year(pix_years) & pix_doy > 366] - 366
-      pix_years[!is.na(pix_doy) & !lubridate::leap_year(pix_years) & pix_doy > 365] <- pix_years[
-        !is.na(pix_doy) & !lubridate::leap_year(pix_years) & pix_doy > 365] + 1
-      pix_doy[!is.na(pix_doy) & !lubridate::leap_year(pix_years) & pix_doy > 365] <- pix_doy[
-        !is.na(pix_doy) & !lubridate::leap_year(pix_years) & pix_doy > 365] - 365
+
+      # doy correction
+      # TODO: Make it FAAAST
+      for (d in 1:length(pix_doy)) {
+        if (lubridate::leap_year(pix_years[d])) {
+          if (!is.na(pix_doy[d])) {
+            if (pix_doy[d] > 366) {
+              pix_years[d] + 1
+              pix_doy[d] - 366
+            }
+          }
+        } else {
+          if (!is.na(pix_doy[d])) {
+            if (pix_doy[d] > 365) {
+              pix_years[d] + 1
+              pix_doy[d] - 365
+            }
+          }
+        }
+      }
+
+      # TODO: try to fix quick and dirty data wrestling below
+      # pix_doy[!is.na(pix_doy) & lubridate::leap_year(pix_years) & pix_doy > 366] <- pix_doy[
+      #   !is.na(pix_doy) & lubridate::leap_year(pix_years) & pix_doy > 366] - 366
+      # pix_years[!is.na(pix_doy) & lubridate::leap_year(pix_years) & pix_doy > 366] <- pix_years[
+      #   !is.na(pix_doy) & lubridate::leap_year(pix_years) & pix_doy > 366] + 1
+      # pix_doy[!is.na(pix_doy) & !lubridate::leap_year(pix_years) & pix_doy > 365] <- pix_doy[
+      #   !is.na(pix_doy) & !lubridate::leap_year(pix_years) & pix_doy > 365] - 365
+      # pix_years[!is.na(pix_doy) & !lubridate::leap_year(pix_years) & pix_doy > 365] <- pix_years[
+      #   !is.na(pix_doy) & !lubridate::leap_year(pix_years) & pix_doy > 365] + 1
+
+      # date conversion
       pix_doystr <- paste0(as.character(pix_years), as.character(pix_doy))
       pix_dates <- as.Date(pix_doystr, format = "%Y%j")
 
-      #print(pix_dates)
+      # check input timeseries
+      pix_vi <- check_ts(pix_vi, config$general$max_data_gap, config$general$internal_min)
+
+      # weight assignment
+      if (isTRUE(config$use_qa)) {
+        weights <- MODIS_summary_qa(pix_qa)
+      } else {
+        weights <- rep(1, length(pix_vi))
+      }
+
+      # spike removal
+      weights <- switch(config$spike$spike_method,
+        Median = spike_median(pix_vi, weights, config$general$n_val_per_year,
+                              config$weights$w_min, config&spike$spike_value),
+        STL    = spike_stl(),
+        STL_w  = spike_stl_w()
+      )
+
+
       #Sys.sleep(1)
-      pix_vi <- rHarmonics::harmonics_fun(pix_vi, pix_dates, 1)
+      #pix_vi <- rHarmonics::harmonics_fun(pix_vi, pix_dates, 1)
 
       # Overwrite the curve with fitted results
       row_vi[j, ] <- pix_vi
@@ -101,6 +152,7 @@ fitTS <- function(prep_dir, out_dir) {
 
     dc_vi[i, ] <- row_vi
   }
+  stop()
   for (i in 1:nlayers(dc_vi)) {
     out_file <- file.path(out_dir, paste0(strsplit(basename(bin_vi_files[i]), '_')[[1]][1],
                                                     "_", strsplit(basename(bin_vi_files), '_')[[1]][2],
