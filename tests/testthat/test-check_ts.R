9f5bf4d9f31bd29e07f07d2d95483fe37c2e5a2d
SandroGroth <sandro_groth@outlook.com>
2020-07-03 22:20:16 +0200

    Added check_ts tests.

---
+++ tests/testthat/test-check_ts.R
@@ -0,0 +1,71 @@
+# .adapt_range -----------------------------------------------------------------------------------------------
+
+y_min = 0
+y_max = 10
+
+# Test ts with NA value
+ts_dummy_na  <- c(1, NA, 10)
+ts_expect_na <- c(1, NA, 10)
+
+# Test with value that exceeds y_max
+ts_dummy_max  <- c(1, NA, 100)
+ts_expect_max <- c(1, NA, 10)
+
+# Test with value below y_min
+ts_dummy_min  <- c(-100, NA, 10)
+ts_expect_min <- c(0, NA, 10)
+
+test_that(".adapt_amp", {
+  expect_equal(.adapt_range(ts_dummy_na, y_min, y_max), ts_expect_na)
+  expect_equal(.adapt_range(ts_dummy_max, y_min, y_max), ts_expect_max)
+  expect_equal(.adapt_range(ts_dummy_min, y_min, y_max), ts_expect_min)
+})
+
+
+# check_ts ---------------------------------------------------------------------------------------------------
+
+valid_min <- 0.5
+w_min <- 0.2
+y_min = 200
+y_max = 10000
+
+# Test with not enough valid values
+ts_y_nvalid <- c(200, NA, 250, NA, NA, NA, 600)
+ts_d_nvalid <- c(1, NA, 3, NA, NA, NA, 5)
+ts_w_nvalid <- c(1, 1, 1, 1, 1, 1, 1)
+ts_expect_nvalid <- list(y=c(NA, NA, NA, NA, NA, NA, NA),
+                          d=c(NA, NA, NA, NA, NA, NA, NA),
+                          w=c(NA, NA, NA, NA, NA, NA, NA))
+
+# Test with NA approximation
+ts_y_na <- c(200, NA, 400, NA, NA, 700, 800)
+ts_d_na <- c(1, 2, 3, 4, 5, 6, 7)
+ts_w_na <- c(1, 1, 1, 1, 1, 1, 1)
+ts_expect_na <- list(y=c(200, 300, 400, 500, 600, 700, 800),
+                     d=c(1, 2, 3, 4, 5, 6, 7),
+                     w=c(1, 0.2, 1, 0.2, 0.2, 1, 1))
+
+# Test with spike approx
+ts_y_spk <- c(100, 200, 300, 100000, 500, -100, 700)
+ts_d_spk <- c(1, 2, 3, 4, 5, 6, 7)
+ts_w_spk <- c(0.2, 1, 1, 0.2, 1, 0.2, 1)
+ts_expect_spk <- list(y=c(200, 200, 300, 400, 500, 600, 700),
+                      d=c(1, 2, 3, 4, 5, 6, 7),
+                      w=c(0.2, 1, 1, 0.2, 1, 0.2, 1))
+
+# test with range adjustment (no spike approx)
+ts_y_rg <- c(100, 200, 300, 100000, 500, -100, 700)
+ts_d_rg <- c(1, 2, 3, 4, 5, 6, 7)
+ts_w_rg <- c(0.2, 1, 1, 0.2, 1, 0.2, 1)
+ts_expect_rg <- list(y=c(200, 200, 300, 10000, 500, 200, 700),
+                     d=c(1, 2, 3, 4, 5, 6, 7),
+                     w=c(0.2, 1, 1, 0.2, 1, 0.2, 1))
+
+test_that("check_ts", {
+  expect_equal(check_ts(ts_y_nvalid, ts_d_nvalid, ts_w_nvalid, valid_min), ts_expect_nvalid)
+  expect_equal(check_ts(ts_y_na, ts_d_na, ts_w_na, valid_min, w_min = 0.2), ts_expect_na)
+  expect_equal(check_ts(ts_y_spk, ts_d_spk, ts_w_spk, valid_min, w_min = 0.2, y_min = y_min, y_max = y_max,
+                        approx_spike = T), ts_expect_spk)
+  expect_equal(check_ts(ts_y_rg, ts_d_rg, ts_w_rg, valid_min, w_min = 0.2, y_min = y_min, y_max = y_max,
+                        approx_spike = F), ts_expect_rg)
+})
