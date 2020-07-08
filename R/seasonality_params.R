52fbcd8220a6c33d6ae6837b53802dcb5eab202a
SandroGroth <sandro_groth@outlook.com>
2020-07-08 17:43:09 +0200

    Initialized pheno params file.

---
+++ R/seasonality_params.R
@@ -0,0 +1,14 @@
+# TODO:
+# - SOS
+# - EOS
+# - LOS
+# - Base value
+# - time middle of season
+# - peak value
+# - amplitude
+# - small integrated value
+# - high integrated value
+
+sos <- function(y, d, method = "SeasonAmp", sos_val = 0.2) {
+  # TODO
+}
